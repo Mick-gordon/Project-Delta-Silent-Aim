60b46ff4d9608290ea5560762705ee690e47bc8a
Mick Gordon <78802703+Mick-gordon@users.noreply.github.com>
2024-12-02 20:38:04 +0000

    Update Main.lua

--- Main.lua
+++ Main.lua
@@ -1,5 +1,5 @@
 -- I Made This Open Source For People To Learn Off.
-if not hookfunction or not newcclosure then  -- Checks If The Executor Has The Required Functions For The Script.
+if not hookfunction or not newcclosure or not getgc then  -- Checks If The Executor Has The Required Functions For The Script.
 	game:GetService("Players").localPlayer:kick("Executor Not Supported"); -- Kicks The Player If Not.
 end;
 
